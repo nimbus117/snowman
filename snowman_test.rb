01e5a673bfda677ab49a78656c5a705cfc5c3256
nimbus117 <mail@nimbus117.co.uk>
2018-08-08 22:29:30 +0100

    finished display_snowman function using case statement for lives left

--- snowman_test.rb
+++ snowman_test.rb
@@ -10,43 +10,47 @@
 #puts ""
 
 
-lives = 6
+def display_snowman(lives_left)
+  case lives_left
+  when 6
+    puts "    _"
+    puts "   (,)"
+    puts "  ( : )"
+    puts "  ( : )"
+    puts ""
+  when 5
+    puts "    _"
+    puts "   (,)"
+    puts "  ( : )"
+    puts "    : )"
+    puts ""
+  when 4
+    puts "    _"
+    puts "   (,)"
+    puts "  ( : )"
+    puts "    :  "
+    puts ""
+  when 3
+    puts "    _"
+    puts "   (,)"
+    puts "    : )"
+    puts "    :  "
+    puts ""
+  when 2
+    puts "    _"
+    puts "   (,)"
+    puts "    :  "
+    puts "    :  "
+    puts ""
+  when 1
+    puts "    _"
+    puts "   (,)"
+    puts ""
+    puts ""
+    puts ""
+  end
+end
 
-case lives
-when 6
-  puts "    _"
-  puts "   (,)"
-  puts "  ( : )"
-  puts "  ( : )"
-  puts ""
-when 5
-  puts "    _"
-  puts "   (,)"
-  puts "  ( : )"
-  puts "  ( : )"
-  puts ""
-when 4
-  puts "    _"
-  puts "   (,)"
-  puts "  ( : )"
-  puts "  ( : )"
-  puts ""
-when 3
-  puts "    _"
-  puts "   (,)"
-  puts "  ( : )"
-  puts "  ( : )"
-  puts ""
-when 2
-  puts "    _"
-  puts "   (,)"
-  puts "  ( : )"
-  puts "  ( : )"
-  puts ""
-when 1
-  puts "    _"
-  puts "   (,)"
-  puts "  ( : )"
-  puts "  ( : )"
-  puts ""
+6.times do |i|
+  display_snowman(i+1)
 end
