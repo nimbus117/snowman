61a6aa3287982e295256d29ca6eac5c8bfb085b8
nimbus117 <mail@nimbus117.co.uk>
2018-08-08 22:22:53 +0100

    testing displaying a snowman

---
+++ snowman_test.rb
@@ -0,0 +1,52 @@
+#puts " O "
+#puts "/|\\"
+#puts "/ \\"
+
+
+#puts "    _"
+#puts "   (,)"
+#puts "  ( : )"
+#puts "  ( : )"
+#puts ""
+
+
+lives = 6
+
+case lives
+when 6
+  puts "    _"
+  puts "   (,)"
+  puts "  ( : )"
+  puts "  ( : )"
+  puts ""
+when 5
+  puts "    _"
+  puts "   (,)"
+  puts "  ( : )"
+  puts "  ( : )"
+  puts ""
+when 4
+  puts "    _"
+  puts "   (,)"
+  puts "  ( : )"
+  puts "  ( : )"
+  puts ""
+when 3
+  puts "    _"
+  puts "   (,)"
+  puts "  ( : )"
+  puts "  ( : )"
+  puts ""
+when 2
+  puts "    _"
+  puts "   (,)"
+  puts "  ( : )"
+  puts "  ( : )"
+  puts ""
+when 1
+  puts "    _"
+  puts "   (,)"
+  puts "  ( : )"
+  puts "  ( : )"
+  puts ""
+end
