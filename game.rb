0b328007260d88a1a40ee43f1221db5a4554ae11
nimbus117 <mail@nimbus117.co.uk>
2018-08-08 21:14:03 +0100

    added/passed remaining game tests

--- game.rb
+++ game.rb
@@ -1,12 +1,27 @@
 class Game
-  attr_reader(:player)
+  attr_reader(:player, :guessed_chars)
 
   def initialize(player, word)
     @player = player
     @word = word
+    @guessed_chars = []
   end
 
   def subtract_life()
     @player.lives -= 1
+  end
+
+  def guess(char)
+    @guessed_chars.push(char)
+    subtract_life() if !@word.test(char)
+  end
+
+  def is_won?()
+    displayed_word = @word.display(@guessed_chars)
+    return !displayed_word.include?('*')
+  end
+
+  def is_lost?()
+    @player.lives == 0
   end
 end
--- specs/game_spec.rb
+++ specs/game_spec.rb
@@ -6,9 +6,10 @@ require_relative('../game.rb')
 
 class GameTest < MiniTest::Test
   def setup
-    word = HiddenWord.new('test word')
+    @word = 'test word'
+    hidden_word = HiddenWord.new(@word)
     @player = Player.new('Bob')
-    @game = Game.new(@player, word)
+    @game = Game.new(@player, hidden_word)
   end
 
   def test_has_player
@@ -18,5 +19,33 @@ class GameTest < MiniTest::Test
   def test_can_subtract_life
     @game.subtract_life()
     assert_equal(5, @player.lives)
+  end
+
+  def test_can_guess__correct
+    @game.guess('t')
+    assert_equal(['t'], @game.guessed_chars)
+    assert_equal(6, @player.lives)
+  end
+
+  def test_can_guess__incorrect
+    @game.guess('x')
+    assert_equal(['x'], @game.guessed_chars)
+    assert_equal(5, @player.lives)
+  end
+
+  def test_is_won
+    @word.each_char {|c| @game.guess(c)}
+    assert_equal(true, @game.is_won?)
+  end
+
+  def test_is_lost
+    'abcfgh'.each_char {|c| @game.guess(c)}
+    assert_equal(true, @game.is_lost?)
+  end
+
+  def test_is_neither_lost_or_won
+    'tes'.each_char {|c| @game.guess(c)}
+    assert_equal(false, @game.is_lost?)
+    assert_equal(false, @game.is_won?)
   end
 end
